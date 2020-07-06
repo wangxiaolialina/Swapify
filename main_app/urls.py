a48c94c7fce3de99497efd79aceb415f0e1348ba
R <50966008+rashmika13@users.noreply.github.com>
2020-07-06 11:59:42 -0400

    index page added for clothing and my clothing

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('clothing/', views.clothings_index, name='index'),
+    path('myclothing/', views.myclothings_index, name='myindex'),
+    path('accounts/signup/', views.signup, name='signup'),
 ]
